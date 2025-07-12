a0e104a4f18a20c6d1d419c1a17325201e8b76b5
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-07-12 21:40:25 +0100

    Rough template of homepage

---
+++ web/src/components/DownloadIcons.tsx
@@ -0,0 +1,8 @@
+
+
+export function DownloadIcons() {
+  return (
+    <h1>Download Icons</h1>
+  )
+}
+
---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { FaGithub } from "react-icons/fa";
+
+export function Navbar() {
+  return (
+    <nav>
+      <ul>Home</ul>
+      <ul>About</ul>
+      <ul>Donate</ul>
+      <FaGithub />
+    </nav>
+  )
+}
+
