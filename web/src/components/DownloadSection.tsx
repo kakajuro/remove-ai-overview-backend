c4a8690ab36b1435e78cdc2700e47573c1228e9d
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-07-13 15:38:55 +0100

    Landing page and navbar done

--- web/src/components/DownloadSection.tsx
+++ web/src/components/DownloadSection.tsx
@@ -5,7 +5,9 @@ import { BlurFade } from "@/components/magicui/blur-fade";
 export function DownloadSection() {
   return (
     <BlurFade direction="up" delay={5}>
-      <button className="mt-24 mb-20 text-3xl font-bold hover:cursor-pointer hover:scale-105 transition-all ease-in-out">Install</button>
+      <button className="mt-24 mb-20 text-3xl font-bold hover:cursor-pointer hover:scale-105 transition-all ease-in-out">
+        <a href="/install">Install</a>
+      </button>
     </BlurFade>
   );
 }
