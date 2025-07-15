68eafe1bfe381f388560fb7dbb10d85c60a02654
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-07-15 14:24:37 +0100

    Added deply script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo Pulling latest changes...
+git pull
+
+echo Pulling latest images...
+docker compose pull
+
+# Restart services
+docker compose up -d --build
+
+# Cleanup
+echo Cleaning unused containers
+docker system prune -a -f
+
+echo Deployment complete
