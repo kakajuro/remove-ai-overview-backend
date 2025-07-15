4970a56027aceeb3f1bce7f435a0627135438e39
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-07-16 00:10:14 +0100

    Update astro build + config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -16,5 +17,9 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   server: { port: 3000, host: true }
 });
