ec5e5b5196fa6f4eb7ae10fdab6cfb6f49bdc297
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-08-05 18:55:52 +0100

    Remove route blocking

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,10 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://removeaioverview.com',
-  redirects: {
-    "/about": "/install",
-    "/donate": "/install"
-  },
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
--- web/src/components/DowloadIcons.tsx
+++ web/src/components/DowloadIcons.tsx
@@ -5,17 +5,17 @@ export function DownloadIcons() {
   return (
     <div className="flex flex-row justify-between mt-8 w-full sm:w-[80%] md:w-[60%] lg:w-[40%]">
       <BlurFade direction="up">
-        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="#" target="_blank">
+        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="https://chromewebstore.google.com/detail/remove-ai-overview/jgiidhkmeobhfhigobkojechgmodclji" target="_blank">
           <FaChrome />
         </a>
       </BlurFade>
       <BlurFade direction="up" delay={0.3}>
-        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="#" target="_blank">
+        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="https://addons.mozilla.org/en-US/firefox/addon/removeaioverviews/" target="_blank">
           <FaFirefoxBrowser />
         </a>
       </BlurFade>
       <BlurFade direction="up" delay={0.6}>
-        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="#" target="_blank">
+        <a className="text-5xl hover:cursor-pointer hover:scale-105 hover:text-gray-200 transition-all ease-in-out" href="https://microsoftedge.microsoft.com/addons/detail/ahhakmjljdfieadnnainepkoneamnabl" target="_blank">
           <FaEdge />
         </a>
       </BlurFade>
