004181348edce4ea5fc8be3b00ada2acfe031480
swishyy <68352336+kakajuro@users.noreply.github.com>
2026-01-20 18:10:40 +0000

    Add "updated" page

---
+++ web/src/components/MyConfetti.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import confetti from "canvas-confetti";
+
+export default function ConfettiFireworks() {
+
+  useEffect(() => {
+    const duration = 2.5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    const randomInRange = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+        return;
+      }
+
+      const particleCount = 20 * (timeLeft / duration);
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return null; // no DOM needed
+}
